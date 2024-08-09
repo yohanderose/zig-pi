cc321aff5b025394d2a8f04bafc2089ee314b25d
Yohan de Rose <yohan.derose@agoda.com>
2024-08-09 18:01:20 +0700

    feat: adding single/dual pwm

--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,12 @@ const c = @cImport({
 });
 
 const LED_PIN = 26;
+const LED_PIN2 = 19;
 
 fn cleanup() void {
     std.debug.print("Exiting ...\n", .{});
     _ = c.gpioSetMode(LED_PIN, c.PI_INPUT);
+    _ = c.gpioSetMode(LED_PIN2, c.PI_INPUT);
     _ = c.gpioTerminate();
     std.process.exit(0);
 }
@@ -18,6 +20,41 @@ fn sigintHandler(sig: c_int) callconv(.C) void {
     std.debug.print("SIGINT received\n", .{});
     _ = sig;
     cleanup();
+}
+
+fn blinky() void {
+    while (true) {
+        _ = c.gpioWrite(LED_PIN, c.PI_HIGH);
+        _ = c.gpioWrite(LED_PIN2, c.PI_LOW);
+        std.time.sleep(300_000_000);
+        _ = c.gpioWrite(LED_PIN, c.PI_LOW);
+        _ = c.gpioWrite(LED_PIN2, c.PI_HIGH);
+        std.time.sleep(300_000_000);
+    }
+}
+
+fn pulsey() void {
+    var i: u32 = 0;
+    // std.debug.print("Resetting previous PWM max {} to 1000", .{c.gpioGetPWMdutycycle(LED_PIN)});
+    const max = 10000;
+    _ = c.gpioSetPWMrange(LED_PIN, max);
+    _ = c.gpioSetPWMrange(LED_PIN2, max);
+
+    while (true) {
+        i = 0;
+
+        while (i < max) : (i += 1) {
+            _ = c.gpioPWM(LED_PIN, i);
+            _ = c.gpioPWM(LED_PIN2, i);
+            std.time.sleep(200_000);
+        }
+
+        while (i > 0) : (i -= 1) {
+            _ = c.gpioPWM(LED_PIN, i);
+            _ = c.gpioPWM(LED_PIN2, i);
+            std.time.sleep(200_000);
+        }
+    }
 }
 
 pub fn main() !void {
@@ -34,13 +71,8 @@ pub fn main() !void {
 
     if (c.gpioInitialise() < 0) return std.debug.print("Failed to initialize pigpio\n", .{});
     _ = c.gpioSetMode(LED_PIN, c.PI_OUTPUT);
+    _ = c.gpioSetMode(LED_PIN2, c.PI_OUTPUT);
 
-    while (true) {
-        _ = c.gpioWrite(LED_PIN, c.PI_HIGH);
-        std.time.sleep(100_000_000);
-        _ = c.gpioWrite(LED_PIN, c.PI_LOW);
-        std.time.sleep(100_000_000);
-    }
-
+    pulsey();
     cleanup();
 }
